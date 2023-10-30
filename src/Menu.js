29cfa4f62a5109701da034742c77c6a07e2e69d3
Emmanuel Guerrero <emanuel.gue@gmail.com>
2023-10-29 22:42:25 -0500

    turning things into functions

--- src/Menu.js
+++ src/Menu.js
@@ -1,3 +1,5 @@
+//I have to add the classes to all the elements i forgot!!
+
 //import pictures
 import horchataIcn from "./horchata.jpg";
 import lemonadeIcn from "./lemonade.jpg";
@@ -15,15 +17,106 @@ export function menu() {
   //body sub-section
   const menuHeadingContainer = document.createElement("div");
   const menuSubHeadingContainer = document.createElement("div");
-  const menuOuter = document.createElement("div");
-  const menuInner = document.createElement("div");
+  /* const menuOuter = document.createElement("div");
+  const menuInner = document.createElement("div"); */
+
+  //makes array of variable names for outer menu divs
+  function menuOuterArray(n) {
+    let newArr = [];
+    for (let i = 0; i < n; i++) {
+      newArry.push("menu-outer" + i);
+    }
+    return newArr;
+  }
+
+  //makes array of variable names for inner menu divs
+  function menuInnerArray(n) {
+    let newArr = [];
+    for (let i = 0; i < n; i++) {
+      newArry.push("menu-inner" + i);
+    }
+    return newArr;
+  }
+
+  //pass div arrays to a variable
+  const menuOuterDivs = menuOuterArray(3);
+  const menuInnerDivs = menuInnerArray(3);
+
+  //make divs each with their own unique name
+  function makeOuterDivs() {
+    let array = [];
+    for (let i = 0; i < menuOuterDivs.length; i++) {
+      menuOuterDivs[i] = document.createElement("div");
+      array.push(menuOuterDivs[i]);
+    }
+
+    return array;
+  }
+
+  //make inner divs
+  function makeInnerDivs() {
+    let array = [];
+    for (let i = 0; i < menuInnerDivs.length; i++) {
+      menuInnerDivs[i] = document.createElement("div");
+      array.push(menuInnerDivs[i]);
+    }
+    return array;
+  }
+
+  //appending h3 and p's to inner
+  function appendPToH3() {
+    //first append p to h3
+    horchata.append(horchataDescr, horchataPrice, horchataImg);
+    lemonade.append(lemonadeDescr, lemonadePrice, lemonadeImg);
+    unsweetTea.append(teaDescr, teaPrice, unSweetTeaImg);
+
+    let array = [horchata, lemonade, unsweetTea];
+
+    return array;
+  }
+
+  //appends h3 & p's to inner menu div
+  function appendToInnerDiv() {
+    let array = [];
+    let divArray = makeInnerDivs();
+    let h3Array = appendPToH3();
+    divArray.forEach((div) => {
+      h3Array.forEach((drink) => {
+        array.push(div.append(drink));
+      });
+    });
+    return array;
+  }
+
+  //append inner menu div to outer menu div
+  function appendInnerToOuterDiv() {
+    let array = [];
+    const innerDivArray = appendToInnerDiv();
+    const outerDivArray = makeOuterDivs();
+    outerDivArray.forEach((outerDiv) => {
+      innerDivArray.forEach((innerDiv) => {
+        array.push(outerDiv.appendChild(innerDiv));
+      });
+    });
+    return array;
+  }
+
+  //append outer menu div to content main content div
+
+  function appendContentToContainer() {
+    const divContent = appendInnerToOuterDiv();
+    divContent.forEach((div) => {
+      mainContent.appendChild(div);
+    });
+    return mainContent;
+  }
 
   //beverage sections
   const mainHeadingTitle = document.createElement("h1");
   const beverageHeader = document.createElement("h2");
   const horchata = document.createElement("h3");
   const lemonade = document.createElement("h3");
-  const UnsweetTea = document.createElement("h3");
+  const unsweetTea = document.createElement("h3");
   const horchataDescr = document.createElement("p");
   const horchataPrice = document.createElement("p");
   const lemonadeDescr = document.createElement("p");
@@ -55,13 +148,7 @@ export function menu() {
     "This tea is not for the light hearted! A nice blend of bitter and sweet you can't decide if you want to put her down! Kinda like your ex.. JK!";
   teaPrice.innetText = "$3";
 
+  //adding class names
+
   //Appending beverage main sections
-
-  const foodHeader = document.createElement("header");
-  const foodSection = document.createElement("section");
-  const food = document.createElement("h2");
-
-  const sidesHeader = document.createElement("header");
-  const sides = document.createElement("h2");
-  const sidesSection = document.createElement("section");
 }
---
+++ src/MenuImproved.js
@@ -0,0 +1,31 @@
+/*import pictures
+import horchataIcn from "./horchata.jpg";
+import lemonadeIcn from "./lemonade.jpg";
+import unsweetIcn from "./unsweat-tea.jpg";
+
+export function menu() {
+    
+    
+    //creates divs
+    
+    const divNames = ["outer-menu", "inner-menu"]
+    function createDiv () {
+        for(i, i < 20, i++) {
+            let div +  = document.createElement("div");
+        }
+    }
+
+
+    //creates h2
+
+    //creates h3
+
+    //creates p's
+
+    //creates img
+
+    //creates classnames
+
+
+}
+*/
